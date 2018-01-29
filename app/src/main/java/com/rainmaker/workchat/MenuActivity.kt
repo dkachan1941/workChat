f34c5f33d3cdbbdc2339331a2d9e3f7e2c69ee17
Dmitry Kachan <kachandl@sibedge.com>
2018-01-29 19:34:11 +0300

    conductor added

--- app/src/main/java/com/rainmaker/workchat/MenuActivity.kt
+++ app/src/main/java/com/rainmaker/workchat/MenuActivity.kt
@@ -5,28 +5,42 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import co.zsmb.materialdrawerkt.builders.accountHeader
 import co.zsmb.materialdrawerkt.builders.drawer
 import co.zsmb.materialdrawerkt.builders.footer
 import co.zsmb.materialdrawerkt.draweritems.badgeable.primaryItem
 import co.zsmb.materialdrawerkt.draweritems.divider
 import co.zsmb.materialdrawerkt.draweritems.profile.profile
+import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Controller
+import com.bluelinelabs.conductor.Router
+import com.bluelinelabs.conductor.RouterTransaction
 import com.mikepenz.materialdrawer.Drawer
+import com.rainmaker.workchat.controllers.ChatRoomsController
+import com.rainmaker.workchat.controllers.HomeController
 import kotlinx.android.synthetic.main.activity_menu.*
 import kotlin.reflect.KClass
 
 class MenuActivity : AppCompatActivity() {
 
     private lateinit var drawerResult: Drawer
+    private lateinit var router: Router
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
-        initViews()
+        initViews(savedInstanceState)
     }
 
-    private fun initViews() {
+    private fun initViews(savedInstanceState: Bundle?) {
         toolbarMenu.setTitle(R.string.menu_title)
+
+        val container = findViewById<ViewGroup>(R.id.frameLayout)
+        router = Conductor.attachRouter(this, container, savedInstanceState)
+        if (!router.hasRootController()){
+            router.setRoot(RouterTransaction.with(HomeController()))
+        }
 
         drawerResult = drawer {
             toolbar = toolbarMenu
@@ -37,7 +51,7 @@ class MenuActivity : AppCompatActivity() {
                 }
             }
             primaryItem(getString(R.string.public_chats)){
-                onClick(openActivity(ChatRoomsActivity::class))
+                onClick(pushController(ChatRoomsController()))
             }
             primaryItem(getString(R.string.private_chats)){
                 onClick(openActivity(ChatListActivity::class))
@@ -65,6 +79,11 @@ class MenuActivity : AppCompatActivity() {
 
     private fun <T : Activity> openActivity(activity: KClass<T>): (View?) -> Boolean = {
         startActivity(Intent(this@MenuActivity, activity.java))
+        false
+    }
+
+    private fun pushController(controller: Controller): (View?) -> Boolean = {
+        router.pushController(RouterTransaction.with(controller))
         false
     }
 
---
+++ app/src/main/java/com/rainmaker/workchat/controllers/HomeController.kt
@@ -0,0 +1,17 @@
+package com.rainmaker.workchat.controllers
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bluelinelabs.conductor.Controller
+
+import com.rainmaker.workchat.R
+
+/**
+ * Created by dmitry on 1/29/18.
+ */
+class HomeController: Controller() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
+        return inflater.inflate(R.layout.controller_home, container, false)
+    }
+}
